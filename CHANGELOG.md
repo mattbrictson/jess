4083100f663ecfb8a3fef9a4ca693686e1f0dff9
Matt Brictson <matt@mattbrictson.com>
2017-01-15 14:29:31 -0800

    Fix ext attrs `to_hash` behavior for awesome_print

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ Jess is in a pre-1.0 state. This means that its APIs and behavior are subject to
 ## [Unreleased][]
 
 * Your contribution here!
+* Fixed a `method_missing` bug in `ExtensionAttributes` that caused problems with `awesome_print`
 
 ## 0.1.0 (2016-10-03)
 
--- lib/jess/extension_attributes.rb
+++ lib/jess/extension_attributes.rb
@@ -3,16 +3,23 @@ require "forwardable"
 module Jess
   # A Hash-like wrapper around the extension attributes that facilitates easy
   # key/value access.
-  class ExtensionAttributes < Resource
+  class ExtensionAttributes
     extend Forwardable
     def_delegators :@values, :[], :fetch, :key?, :keys, :size, :length, :to_h
 
     def initialize(json)
-      super
       @values = json.each_with_object({}) do |attr, hash|
         hash[attr.name] = attr.value
       end
       @values.freeze
+    end
+
+    # Explicitly delegate instead of using def_delegators in order to be
+    # compatible with awesome_print. The original Hash#to_hash method is
+    # implemented in C, which means it has an arity of -1. This confuses
+    # awesome_print.
+    def to_hash
+      @values.to_hash
     end
   end
 end
