b9c5433bac40590b705b415ed83d88d92f3aea56
Matt Brictson <matt@mattbrictson.com>
2018-03-31 11:54:28 -0700

    RuboCop 0.54.0

--- test/support/json_fixtures.rb
+++ test/support/json_fixtures.rb
@@ -2,7 +2,7 @@ require "json"
 
 module JSONFixtures
   def json_fixture(name, raw: false)
-    fix_dir = File.expand_path("../../fixtures", __FILE__)
+    fix_dir = File.expand_path("../fixtures", __dir__)
     json = IO.read(File.join(fix_dir, "#{name}.json"))
     raw ? json : JSON.parse(json)
   end
